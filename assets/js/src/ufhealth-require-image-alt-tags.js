517d6609709042aa19f2b508ae68995ebd76bcf0
Chris Wiegman <cwiegman@ufl.edu>
2017-06-15 10:32:01 -0400

    Correctly handle multiple images.

--- assets/js/src/ufhealth-require-image-alt-tags.js
+++ assets/js/src/ufhealth-require-image-alt-tags.js
@@ -10,33 +10,62 @@ jQuery(document).ready(function ($) {
 
 	var checkForAlt = function (showNotice) {
 
-		var notice   = ('undefined' !== typeof showNotice) ? showNotice : false;
-		var $altText = $('.media-modal-content label[data-setting="alt"] input');
-		var $parent  = $('.media-frame-toolbar .media-toolbar-primary');
+		var notice         = ('undefined' !== typeof showNotice) ? showNotice : false,
+		    $parent        = $('.media-frame-toolbar .media-toolbar-primary'),
+		    selectedImages = $('.selection-view ul.attachments li'),
+		    canProceed     = true,
+		    badImages      = [];
 
-		if (!$altText.length) {
-			// No image selected in the first place; bail out.
-			return;
-		}
+		selectedImages.each(function (idx, li) {
 
-		if ($altText.val().length) {
-			$parent.addClass('my-has-alt-text');
-			return true;
-		} else {
-			$parent.removeClass('my-has-alt-text');
-			if (notice) {
-				alert('Missing Alt Text!');
+			var $image  = $(li),
+			    imageId = $image.attr('data-id'),
+			    image   = wp.media.model.Attachment.get(imageId),
+			    altText = image.get('alt');
+
+			console.log(altText);
+
+			if (altText.length) {
+
+				$parent.addClass('ufh-has-alt-text');
+				$image.removeClass('ufh-needs-alt-text');
+
+			} else {
+
+				$image.addClass('ufh-needs-alt-text');
+
+				badImages.push(image.get('title'));
+
+				canProceed = false;
+
 			}
+
+		});
+
+		if (false === canProceed) {
+
+			$parent.removeClass('ufh-has-alt-text');
+
+			if (notice) {
+
+				var imageList = '\n\n';
+
+				for (var i = 0, l = badImages.length; i < l; i++) {
+					imageList = imageList + badImages[i];
+				}
+
+				alert('The following images are missing alt text: ' + imageList);
+			}
+
 			return false;
+
 		}
+
+		return true;
+
 	};
 
 	var body = $('body');
-
-	// Bind to change.
-	body.on('DOMSubtreeModified', '.selection-view ul.attachments', function () {
-		console.log(wp.media);
-	});
 
 	// Bind to keyup.
 	body.on('keyup', '.media-modal-content label[data-setting="alt"] input', function () {
