8c7c77b995ab19e4a920babdfcdaa05aa1fa56e4
Chris Wiegman <cwiegman@ufl.edu>
2017-06-15 10:38:59 -0400

    Fix JS hint error in string

--- assets/js/src/ufhealth-require-image-alt-tags.js
+++ assets/js/src/ufhealth-require-image-alt-tags.js
@@ -54,7 +54,7 @@ jQuery(document).ready(function ($) {
 					imageList = imageList + badImages[i];
 				}
 
-				alert('The following images are missing alt text: ' + imageList + "\n");
+				alert('The following images are missing alt text: ' + imageList + '\n');
 			}
 
 			return false;
