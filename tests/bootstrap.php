f4e173bb8fce220b9b40e7444688e531d051fdf4
Chris Wiegman <cwiegman@ufl.edu>
2018-07-20 14:31:28 -0400

    Fix file path

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -24,7 +24,7 @@ require_once $_tests_dir . '/includes/functions.php';
  */
 function _manually_load_plugin() {
 
-	require dirname( dirname( __FILE__ ) ) . '/uf-health-test.php';
+	require dirname( dirname( __FILE__ ) ) . '/ufhealth-require-image-alt-tags.php';
 }
 
 tests_add_filter( 'muplugins_loaded', '_manually_load_plugin' );
